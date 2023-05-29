6d4dd73275e2cb3f347e0197cf12728c8ffb20c5
parom <E.Parominskiy@isida.by>
2023-05-29 11:49:19 +0300

    SOLID - single responsibility principle (SRP) fixed

--- src/SOLID/single_responsibility/SingleResponsibility.java
+++ src/SOLID/single_responsibility/SingleResponsibility.java
@@ -1,6 +1,8 @@
 package SOLID.single_responsibility;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.ArrayList;
@@ -25,25 +27,45 @@ class Journal {
         return String.join(System.lineSeparator(), entries);
     }
 
-    public void save(String filename) throws FileNotFoundException {
-        try (PrintStream out = new PrintStream(filename)) {
-            out.println(this);
-        }
-    }
-
-    public void load(String filename) {
-    }
-
-    public void load(URL url) {
-    }
+//    public void save(String filename) throws FileNotFoundException {
+//        try (PrintStream out = new PrintStream(filename)) {
+//            out.println(this);
+//        }
+//    }
+//
+//    public void load(String filename) {
+//    }
+//
+//    public void load(URL url) {
+//    }
 
 }
 
+class Persistence {
+    public void saveToFile(Journal journal, String filename, boolean overwrite) throws FileNotFoundException {
+        if (overwrite || new File(filename).exists()) {
+            try (PrintStream out = new PrintStream(filename)) {
+                out.println(journal.toString());
+            }
+        }
+    }
+
+    public Journal load(String filename) { return null; }
+    public Journal load(URL url) { return null; }
+}
+
 class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Journal j = new Journal();
         j.addEntry("I cried today");
         j.addEntry("I ate a bug");
         System.out.println(j);
+
+        Persistence p = new Persistence();
+        String filename = "journal.txt";
+        p.saveToFile(j, filename, true);
+
+        Runtime.getRuntime().exec("nano " + filename);
+
     }
 }
