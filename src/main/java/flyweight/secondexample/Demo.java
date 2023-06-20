0ad223390b7c5df64f1dec8e1ed8626ec148e761
parom <E.Parominskiy@isida.by>
2023-06-20 08:09:28 +0300

    Flyweight betterFormatter

--- src/main/java/flyweight/secondexample/Demo.java
+++ src/main/java/flyweight/secondexample/Demo.java
@@ -1,5 +1,8 @@
 package flyweight.secondexample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author E.Parominsky 16/06/2023 08:34
  */
@@ -30,10 +33,60 @@ class FormattedText {
     }
 }
 
+class BetterFormattedText {
+
+    private String plainText;
+    private List<TextRange> formatting = new ArrayList<>();
+
+    public BetterFormattedText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    public TextRange getRange(int start, int end) {
+        TextRange range = new TextRange(start, end);
+        formatting.add(range);
+        return range;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < plainText.length(); ++i) {
+            char c = plainText.charAt(i);
+            for (TextRange range : formatting) {
+                if (range.covers(i) && range.capitalize) {
+                    c = Character.toUpperCase(c);
+                }
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public class TextRange {
+        public int start, end;
+        public boolean capitalize, bold, italic;
+
+        public TextRange(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public boolean covers(int position) {
+            return position >= start && position <= end;
+        }
+    }
+}
+
 class Demo {
     public static void main(String[] args) {
         FormattedText ft = new FormattedText("This is a brave new world");
         ft.capitalize(10, 15);
         System.out.println(ft);
+
+        BetterFormattedText bft = new BetterFormattedText("Make America Great Again");
+        bft.getRange(13, 18).capitalize = true;
+        System.out.println(bft);
+
     }
 }
